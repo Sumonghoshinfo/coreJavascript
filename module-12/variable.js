ea8b6c804215618b343fbe28e6a1d4c916f844e3
sumon <sumonghoshinfo@gmail.com>
2022-12-27 00:27:33 +0600

    variable

---
+++ module-12/variable.js
@@ -0,0 +1,31 @@
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z);
+
+var bananaPrice = 12;
+console.log(bananaPrice);
+console.log(typeof bananaPrice);
+
+var name = "sumon ghosh"
+console.log(name);
+console.log(typeof name);
+
+var isHot = true;
+var isRich = false;
+console.log(isHot);
+console.log(typeof isHot);
+
+// variable type  string,number
+
+//  variable name case sentive
+
+
+var name = "Hero Alom";
+var name = " Shakib Khan";
+console.log(name);
+
+var name = `hero alom`;
+console.log(name);
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
