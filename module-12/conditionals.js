146fa5f37f310229af7fb814ccc163318ffb60de
sumon <sumonghoshinfo@gmail.com>
2022-12-27 21:49:12 +0600

    mpdule12

---
+++ module-12/conditionals.js
@@ -0,0 +1,19 @@
+// if (){
+
+
+// }
+
+// else{
+
+
+// }
+
+var biscuitPrice =12
+if (biscuitPrice < 10){
+    console.log("I will eat the biscuits");
+}
+else{
+    console.log("Khali cha is better")
+}
+
+
--- module-12/variable.js
+++ module-12/variable.js
@@ -29,3 +29,31 @@ var name = `hero alom`;
 console.log(name);
 console.log(name.toLowerCase());
 console.log(name.toUpperCase());
+// number
+var number1 = 25;
+var number1 = ''+number1;
+console.log(typeof number1);
+var number2 ='25.5'
+var number3 =parseFloat(number2);
+var number3 =parseInt(number2);
+console.log(number3)
+result = number1 + number3
+console.log(result);
+
+// var number6 = 0.1;
+// var numver7 = 0.2;
+// console.log(total);
+
+
+
+// parseFloat ,parseInt,
+
+// Math
+
+var number = -5;
+var absoluteNumber = Math.abs(number);
+
+// math.round(number)
+// Math.ceil(number)
+// Math.floor(number)
+console.log(absoluteNumber);
